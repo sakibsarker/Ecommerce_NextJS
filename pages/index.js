d36088e1e69fb2b107f99c96c455147e046940aa
sakisarker <sakib15-3824@diu.edu.bd>
2022-11-09 00:57:27 +0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
